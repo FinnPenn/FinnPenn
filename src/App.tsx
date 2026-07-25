898b1e88f32e8b6467852ce5a0fe31e112b068bc
FinnPenn <finnpenning@hotmail.de>
2026-07-25 02:34:08 +0200

    feat: add hero background gradient
    Adds interactive background gradient from React Bits with custom blur effect

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,48 @@ import { Layout } from '@/components/layout'
 import { AssetDeck } from './components/asset-deck'
 import  { ReactLenis, useLenis } from 'lenis/react'
 import VideoScrollPage from './components/experimental/video-scroll-page'
+import GradientBlinds from './components/GradientBlinds'
 
 function App() {
    const lenis = useLenis((lenis) => {
     console.log(lenis)
   })
   return (
+    // <ReactLenis root>
+    //   <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    //       <Layout>
+    //         <Header/>
+    //           <div className='h-[5000px]'>
+    //             <section id="hero">
+    //               <AssetDeck/>
+    //               <VideoScrollPage/>
+    //             </section>
+    //           </div>
+    //         </Layout>
+    //   </ThemeProvider>
+    // </ReactLenis>
     <ReactLenis root>
-      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-          <Layout>
-            <Header/>
-              <div className='h-[5000px]'>
-                <section id="hero">
-                  <AssetDeck/>
-                  <VideoScrollPage/>
-                </section>
-              </div>
-            </Layout>
-      </ThemeProvider>
-    </ReactLenis>
+      <div style={{ width: '100%', height: '100vh', position: 'relative', background: '#120F17' }}>
+        <GradientBlinds
+          className='blur-3xl'
+          gradientColors={["#F349D4","#6230bb"]}
+          angle={20}
+          noise={0.5}
+          blindCount={16}
+          blindMinWidth={60}
+          mouseDampening={0.5}
+          mirrorGradient={false}
+          spotlightRadius={0.55}
+          spotlightSoftness={1}
+          spotlightOpacity={1}
+          distortAmount={0}
+          shineDirection="left"
+        />
+      </div>
+      <div className='bg-[#120F17] text-white h-500'>
+      testset
+      </div>
+      </ReactLenis>
   )
 }
 
