9c6befb83880a58fb96421c23d1e34bcc246c8e3
FinnPenn <finnpenning@hotmail.de>
2026-07-26 14:22:41 +0200

    feat: hero text & description

--- src/App.tsx
+++ src/App.tsx
@@ -26,8 +26,8 @@ function App() {
     //         </Layout>
     //   </ThemeProvider>
     // </ReactLenis>
-    <ReactLenis root>
-      <div style={{ width: '100%', height: '100vh', position: 'relative', background: '#120F17' }}>
+    <ReactLenis root >
+      <div className='w-full h-screen relative bg-[#120F17]'>
         <GradientBlinds
           className='blur-3xl'
           gradientColors={["#F349D4","#6230bb"]}
@@ -43,11 +43,26 @@ function App() {
           distortAmount={0}
           shineDirection="left"
         />
+        <div className='flex h-screen w-full absolute bg-linear-to-r from-[#E6CDF9] to-[#FF0FBF] bg-clip-text text-transparent justify-center items-center top-0 left-0 pointer-events-none'>
+          <div className='w-min relative h-min'>
+            <div className='font-bold text-[80px] tracking-[8%] absolute top-35 right-5 text-shadow-[4px_4px_4px_0px_rgba(0,0,0,0.25)]'>hello, I am</div>
+            <div className='text-[400px] font-black text-shadow-[4px_4px_4px_0px_rgba(0,0,0,0.25)]'>finn</div>
+            <div className='text-[48px] tracking-[-4%] absolute bottom-20 right-4 text-shadow-[4px_4px_4px_0px_rgba(0,0,0,0.25)]'>&#123;aka senix&#125;</div>
+          </div>
+        </div>
       </div>
-      <div className='bg-[#120F17] text-white h-500'>
-      testset
+      <div className='flex w-full justify-center bg-[#120F17] py-80'>
+        <div className='relative h-min'>
+          <div className='z-10 relative font-bold text-[130px] tracking-[-5%] leading-[106%] text-center text-[#D8BFD8]'>
+            Follow me on my <br/> journey as I <br/> advance my skills.
+          </div>
+          <span className='z-20 absolute left-32 -top-8 uppercase px-8 py-5 bg-[#F36DDC] font-black tracking-[8%] text-[20px] transform -rotate-10 shadow-[4px_4px_4px_0px_rgba(0,0,0,0.25)]'>Materials</span>
+          <span className='z-20 absolute right-20 top-29  uppercase px-8 py-5 bg-[#F36DDC] font-black tracking-[8%] text-[20px] transform -rotate-10 shadow-[4px_4px_4px_0px_rgba(0,0,0,0.25)]'>3D-modeling</span>
+          <span className='z-20 absolute top-97 -right-10 uppercase px-8 py-5 bg-[#F36DDC] font-black tracking-[8%] text-[20px] transform -rotate-10 shadow-[4px_4px_4px_0px_rgba(0,0,0,0.25)]'>Frontend</span>
+          <span className='z-0 absolute top-96 left-32 uppercase px-8 py-5 bg-[#F36DDC] font-black tracking-[8%] text-[20px] transform rotate-10 shadow-[4px_4px_4px_0px_rgba(0,0,0,0.25)]'>Stories</span>
+        </div>
       </div>
-      </ReactLenis>
+    </ReactLenis>
   )
 }
 
