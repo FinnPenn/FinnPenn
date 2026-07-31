1cddae86bf6edd2bd3789aae6f4400255fefee24
FinnPenn <finnpenning@hotmail.de>
2026-07-31 15:41:16 +0200

    refactor: remove unneeded code

--- client/src/components/sections/hero.tsx
+++ client/src/components/sections/hero.tsx
@@ -23,9 +23,9 @@ function Hero() {
         />
         <div className='flex h-screen w-full absolute bg-linear-to-r from-[#E6CDF9] to-[#FF0FBF] bg-clip-text text-transparent justify-center items-center top-0 left-0 pointer-events-none'>
             <div className='w-min relative h-min'>
-                <div className='font-bold text-[80px] tracking-[8%] absolute top-35 right-5 text-shadow-[4px_4px_4px_0px_rgba(0,0,0,0.25)]'>hello, I am</div>
-                <div className='text-[400px] font-black text-shadow-[4px_4px_4px_0px_rgba(0,0,0,0.25)]'>finn</div>
-                <div className='text-[48px] tracking-[-4%] absolute bottom-20 right-4 text-shadow-[4px_4px_4px_0px_rgba(0,0,0,0.25)]'>&#123;aka senix&#125;</div>
+                <div className='font-bold text-[80px] tracking-[8%] absolute top-35 right-5'>hello, I am</div>
+                <div className='text-[400px] font-black'>finn</div>
+                <div className='text-[48px] tracking-[-4%] absolute bottom-20 right-3'>&#123;aka senix&#125;</div>
             </div>
         </div>
     </section>
